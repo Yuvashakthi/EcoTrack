7f8a93193e16887c9e9e20d9a4c869c8331f9e16
KiddoGirl <manjubashini2110@gmail.com>
2024-09-22 00:03:22 +0530

    index,governmen,js,css

---
+++ EcoTrack/js/energy.js
@@ -0,0 +1,33 @@
+const ctx = document.getElementById('myPieChart').getContext('2d');
+const myPieChart = new Chart(ctx, {
+    type: 'pie',
+    data: {
+        labels: ['Good Products', 'Schemes', 'Programs', 'Community Involvement'],
+        datasets: [{
+            label: 'User Engagement',
+            data: [60.0, 20.0, 15.0, 5.0],
+            backgroundColor: [
+                'rgba(75, 0, 130, 0.6)',  
+                'rgba(255, 0, 127, 0.6)',   
+                'rgba(255, 140, 0, 0.6)',   
+                'rgba(255, 215, 0, 0.6)'    
+            ],
+            borderColor: [
+                'rgba(75, 0, 130, 1)',  
+                'rgba(255, 0, 127, 1)',   
+                'rgba(255, 140, 0, 1)',   
+                'rgba(255, 215, 0, 1)'    
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'top',
+            },
+        }
+    }
+});
+
---
+++ EcoTrack/js/waste.js
@@ -0,0 +1,33 @@
+const ctx = document.getElementById('myPieChart').getContext('2d');
+const myPieChart = new Chart(ctx, {
+    type: 'pie',
+    data: {
+        labels: ['Good Products', 'Schemes', 'Programs', 'Community Involvement'],
+        datasets: [{
+            label: 'User Engagement',
+            data: [54.9, 24.6, 12.3, 8.2],
+            backgroundColor: [
+                'rgba(75, 0, 130, 0.6)',  
+                'rgba(255, 0, 127, 0.6)',   
+                'rgba(255, 140, 0, 0.6)',   
+                'rgba(255, 215, 0, 0.6)'    
+            ],
+            borderColor: [
+                'rgba(75, 0, 130, 1)',  
+                'rgba(255, 0, 127, 1)',   
+                'rgba(255, 140, 0, 1)',   
+                'rgba(255, 215, 0, 1)'    
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'top',
+            },
+        }
+    }
+});
+
